0e2be65bf12867f0778af61d6973e47f6a2c60ef
kangear <kangear@163.com>
2014-10-24 19:10:40 +0800

    add .gitignore, del /bin path

--- src/com/example/testkey/MainActivity.java
+++ src/com/example/testkey/MainActivity.java
@@ -12,141 +12,141 @@ import android.view.Menu;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
-	private final static String LOG_TAG = "com.example.testkey";
-	TextView tv;
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-		
-		tv = (TextView)findViewById(R.id.textView);
-		IntentFilter filter = new IntentFilter(Intent.ACTION_SCREEN_OFF);
-		// Power
-		registerReceiver(mBatInfoReceiver, filter);
-		// Home
-		final IntentFilter homeFilter = new IntentFilter(Intent.ACTION_CLOSE_SYSTEM_DIALOGS);
-		registerReceiver(homePressReceiver, homeFilter);
+    private final static String LOG_TAG = "com.example.testkey";
+    TextView tv;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-		printToast("开始测试按键！");
-	}
-	
-	@Override
-	public void onDestroy() {
-		// TODO Auto-generated method stub
-		super.onDestroy();
-		// 解除注册 Power
-		if (mBatInfoReceiver != null) {
-			try {
-				unregisterReceiver(mBatInfoReceiver);
-			} catch (Exception e) {
-				Log.e(LOG_TAG, "unregisterReceiver mBatInfoReceiver failure :"
-						+ e.getCause());
-			}
-		};
-		// 解除注册 Home
-		if (homePressReceiver != null) {
-			try {
-				unregisterReceiver(homePressReceiver);
-			} catch (Exception e) {
-				Log.e(LOG_TAG,"unregisterReceiver homePressReceiver failure :"
-								+ e.getCause());
-			}
-		};
-	}
-	
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
-	
-	@Override
+        tv = (TextView)findViewById(R.id.textView);
+        IntentFilter filter = new IntentFilter(Intent.ACTION_SCREEN_OFF);
+        // Power
+        registerReceiver(mBatInfoReceiver, filter);
+        // Home
+        final IntentFilter homeFilter = new IntentFilter(Intent.ACTION_CLOSE_SYSTEM_DIALOGS);
+        registerReceiver(homePressReceiver, homeFilter);
+
+        printToast("开始测试按键！");
+    }
+
+    @Override
+    public void onDestroy() {
+        // TODO Auto-generated method stub
+        super.onDestroy();
+        // 解除注册 Power
+        if (mBatInfoReceiver != null) {
+            try {
+                unregisterReceiver(mBatInfoReceiver);
+            } catch (Exception e) {
+                Log.e(LOG_TAG, "unregisterReceiver mBatInfoReceiver failure :"
+                        + e.getCause());
+            }
+        };
+        // 解除注册 Home
+        if (homePressReceiver != null) {
+            try {
+                unregisterReceiver(homePressReceiver);
+            } catch (Exception e) {
+                Log.e(LOG_TAG,"unregisterReceiver homePressReceiver failure :"
+                                + e.getCause());
+            }
+        };
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
     protected void onUserLeaveHint() {
             //super.onUserLeaveHint();
             System.out.println("onUserLeaveHint");
 
-	}
-	
-	@Override
-	public boolean onKeyDown(int keyCode, KeyEvent event) {   
-	    switch (keyCode) {
-	    
-    		case KeyEvent.KEYCODE_POWER :
-	    		//监控/拦截/屏蔽电源键 这里拦截不了
-	    		printToast("get Key KEYCODE_POWER(KeyCode:"+keyCode+")");
-	    		break;	    	
-    		
-    		case KeyEvent.KEYCODE_RIGHT_BRACKET :
-	    		//监控/拦截/屏蔽返回键
-	    		printToast("get Key KEYCODE_RIGHT_BRACKET");
-	    		break;
-	    		
-	    	case KeyEvent.KEYCODE_MENU :
-	    		//监控/拦截菜单键
-	    		printToast("get Key KEYCODE_MENU");
-	    		break;
-	    		
-	    	case KeyEvent.KEYCODE_HOME:
-	    		//由于Home键为系统键，此处不能捕获
-	    		printToast("get Key KEYCODE_HOME");
-	    		break;
-	    		
-	    	case KeyEvent.KEYCODE_DPAD_UP:
-	    		//监控/拦截/屏蔽上方向键
-	    		printToast("get Key KEYCODE_DPAD_UP");
-	    		break;
-	    		
-	    	case KeyEvent.KEYCODE_DPAD_LEFT:
-	    		//监控/拦截/屏蔽左方向键
-	    		printToast("get Key KEYCODE_DPAD_LEFT");
-	    		break;
-	    		
-	    	case KeyEvent.KEYCODE_DPAD_RIGHT:
-	    		//监控/拦截/屏蔽右方向键
-	    		printToast("get Key KEYCODE_DPAD_RIGHT");
-	    		break;
-	    		
-	    	case KeyEvent.KEYCODE_DPAD_DOWN:
-	    		//监控/拦截/屏蔽下方向键
-	    		printToast("get Key KEYCODE_DPAD_DOWN");
-	    		break;
-	    		
-	    	case KeyEvent.KEYCODE_DPAD_CENTER:
-	    		//监控/拦截/屏蔽中方向键
-	    		printToast("get Key KEYCODE_DPAD_CENTER");
-	    		break;
-	    		
-	    	case KeyEvent.FLAG_KEEP_TOUCH_MODE:
-	    		//监控/拦截/屏蔽长按
-	    		printToast("get Key FLAG_KEEP_TOUCH_MODE");
-	    		break;
-	    		
-	    		
-	    	case KeyEvent.KEYCODE_VOLUME_DOWN:
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        switch (keyCode) {
+
+            case KeyEvent.KEYCODE_POWER :
+                //监控/拦截/屏蔽电源键 这里拦截不了
+                printToast("get Key KEYCODE_POWER(KeyCode:"+keyCode+")");
+                break;
+
+            case KeyEvent.KEYCODE_RIGHT_BRACKET :
+                //监控/拦截/屏蔽返回键
+                printToast("get Key KEYCODE_RIGHT_BRACKET");
+                break;
+
+            case KeyEvent.KEYCODE_MENU :
+                //监控/拦截菜单键
+                printToast("get Key KEYCODE_MENU");
+                break;
+
+            case KeyEvent.KEYCODE_HOME:
+                //由于Home键为系统键，此处不能捕获
+                printToast("get Key KEYCODE_HOME");
+                break;
+
+            case KeyEvent.KEYCODE_DPAD_UP:
+                //监控/拦截/屏蔽上方向键
+                printToast("get Key KEYCODE_DPAD_UP");
+                break;
+
+            case KeyEvent.KEYCODE_DPAD_LEFT:
+                //监控/拦截/屏蔽左方向键
+                printToast("get Key KEYCODE_DPAD_LEFT");
+                break;
+
+            case KeyEvent.KEYCODE_DPAD_RIGHT:
+                //监控/拦截/屏蔽右方向键
+                printToast("get Key KEYCODE_DPAD_RIGHT");
+                break;
+
+            case KeyEvent.KEYCODE_DPAD_DOWN:
                 //监控/拦截/屏蔽下方向键
-	    		printToast("get Key KEYCODE_VOLUME_DOWN(KeyCode:"+keyCode+")");
-	    		break;
-	    		
-	    	case KeyEvent.KEYCODE_VOLUME_UP:
+                printToast("get Key KEYCODE_DPAD_DOWN");
+                break;
+
+            case KeyEvent.KEYCODE_DPAD_CENTER:
                 //监控/拦截/屏蔽中方向键
-	    		printToast("get Key KEYCODE_VOLUME_UP(KeyCode:"+keyCode+")");
-	    		break;
-	    		
-	    	case 220:
-	    	//case KeyEvent.KEYCODE_BRIGHTNESS_DOWN:
+                printToast("get Key KEYCODE_DPAD_CENTER");
+                break;
+
+            case KeyEvent.FLAG_KEEP_TOUCH_MODE:
+                //监控/拦截/屏蔽长按
+                printToast("get Key FLAG_KEEP_TOUCH_MODE");
+                break;
+
+
+            case KeyEvent.KEYCODE_VOLUME_DOWN:
+                //监控/拦截/屏蔽下方向键
+                printToast("get Key KEYCODE_VOLUME_DOWN(KeyCode:"+keyCode+")");
+                break;
+
+            case KeyEvent.KEYCODE_VOLUME_UP:
+                //监控/拦截/屏蔽中方向键
+                printToast("get Key KEYCODE_VOLUME_UP(KeyCode:"+keyCode+")");
+                break;
+
+            case 220:
+            //case KeyEvent.KEYCODE_BRIGHTNESS_DOWN:
                 //监控/拦截/屏蔽亮度减键
-	    		printToast("get Key KEYCODE_BRIGHTNESS_DOWN(KeyCode:"+keyCode+")");
-	    		break;
-	    		
-	    	case 221:	
-	    	//case KeyEvent.KEYCODE_BRIGHTNESS_UP:
+                printToast("get Key KEYCODE_BRIGHTNESS_DOWN(KeyCode:"+keyCode+")");
+                break;
+
+            case 221:
+            //case KeyEvent.KEYCODE_BRIGHTNESS_UP:
                 //监控/拦截/屏蔽亮度加键
-	    		printToast("get Key KEYCODE_BRIGHTNESS_UP(KeyCode:"+keyCode+")");
-	    		break; 				
+                printToast("get Key KEYCODE_BRIGHTNESS_UP(KeyCode:"+keyCode+")");
+                break;
 
 
-	        case KeyEvent.KEYCODE_MEDIA_PLAY:
+            case KeyEvent.KEYCODE_MEDIA_PLAY:
                 printToast("get Key KEYCODE_MEDIA_PLAY(KeyCode:"+keyCode+")");
                 break;
 
@@ -166,50 +166,49 @@ public class MainActivity extends Activity {
                 printToast("get Key KEYCODE_MEDIA_NEXT(KeyCode:"+keyCode+")");
                 break;
 
-	    	default :
-	    		printToast("keyCode: "+keyCode+" (http://developer.android.com/reference/android/view/KeyEvent.html)");
-	    		break;
-	    		
-	    }
-	    
-	    return true;
-	    //return super.onKeyDown(keyCode, event);
-	}
-	
-	public void printToast(String str) {
-		tv.setText(str);	
-	}
+            default :
+                printToast("keyCode: "+keyCode+" (http://developer.android.com/reference/android/view/KeyEvent.html)");
+                break;
 
-	private final BroadcastReceiver homePressReceiver = new BroadcastReceiver() {
-		final String SYSTEM_DIALOG_REASON_KEY = "reason";
-		final String SYSTEM_DIALOG_REASON_HOME_KEY = "homekey";
+        }
 
-		@Override
-		public void onReceive(Context context, Intent intent) {
-			String action = intent.getAction();
-			if (action.equals(Intent.ACTION_CLOSE_SYSTEM_DIALOGS)) {
-				String reason = intent.getStringExtra(SYSTEM_DIALOG_REASON_KEY);
-				if (reason != null
-						&& reason.equals(SYSTEM_DIALOG_REASON_HOME_KEY)) {
-					// 自己随意控制程序，关闭...
-				}
-			}
-		}
+        return true;
+        //return super.onKeyDown(keyCode, event);
+    }
 
-	};
-	private final BroadcastReceiver mBatInfoReceiver = new BroadcastReceiver() {
+    public void printToast(String str) {
+        tv.setText(str);
+    }
 
-		@Override
-		public void onReceive(final Context context, final Intent intent) {
+    private final BroadcastReceiver homePressReceiver = new BroadcastReceiver() {
+        final String SYSTEM_DIALOG_REASON_KEY = "reason";
+        final String SYSTEM_DIALOG_REASON_HOME_KEY = "homekey";
 
-			final String action = intent.getAction();
-			if (Intent.ACTION_SCREEN_OFF.equals(action)) {
-				
-				printToast("get Key KEYCODE_POWER(KeyCode:26)");;
-				// 退出程序...
-			}
-		}
-	};
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if (action.equals(Intent.ACTION_CLOSE_SYSTEM_DIALOGS)) {
+                String reason = intent.getStringExtra(SYSTEM_DIALOG_REASON_KEY);
+                if (reason != null
+                        && reason.equals(SYSTEM_DIALOG_REASON_HOME_KEY)) {
+                    // 自己随意控制程序，关闭...
+                }
+            }
+        }
+
+    };
+    private final BroadcastReceiver mBatInfoReceiver = new BroadcastReceiver() {
+
+        @Override
+        public void onReceive(final Context context, final Intent intent) {
+
+            final String action = intent.getAction();
+            if (Intent.ACTION_SCREEN_OFF.equals(action)) {
+
+                printToast("get Key KEYCODE_POWER(KeyCode:26)");;
+                // 退出程序...
+            }
+        }
+    };
 
 }
-
